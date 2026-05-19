23ad5be074016eb3c194a79169bd928d53d2fec4
Marti <marti@llamarisk.com>
2026-05-19 21:03:32 +0200

    deploy: tab spacing

--- src/data/graphql/client.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLClient } from 'graphql-request';
-
-const GQL_URL = import.meta.env.VITE_AAVE_GRAPHQL || 'https://api.v4.aave.com/graphql';
-
-export const gql = new GraphQLClient(GQL_URL);
